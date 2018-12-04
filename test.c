ab90469508133ee353dde8c30597aaee84f5e5eb
Allison Robbins 2018 <allisonrobbins@Allisons-MacBook-Pro.local>
2018-12-03 23:09:46 -0500

    computer science is hard

--- test.c
+++ test.c
@@ -3,18 +3,15 @@
 #include <stdlib.h>
 #include "hashmap.h"
 
-
-void readFiles(struct hashmap *hm);
-void readFiles(struct hashmap* hm)
+void training(struct hashmap *hm);
+void training(struct hashmap* hm)
 {
+    printf("made it here");
     FILE *fptr;
     char line[256];
     char* word;
     fptr = fopen("D1.txt", "r");//open file 1
-    if(fptr == NULL)
-    {
-        return;
-    }
+    printf("opened file 1");
     fgets(line,256,fptr);
     word = strtok(line, " ");
     while(1)//the loop to read file D1.txt into the hashmap
@@ -23,8 +20,9 @@ void readFiles(struct hashmap* hm)
         {
             break;
         }
+        printf(" %s",word);
         hm_put(hm,word,1,0,0);
-        char* word = strtok(NULL," ");
+        word = strtok(NULL," ");
     }
     fclose(fptr);//close file 1
     fptr = fopen("D2.txt", "r");//open file 2
@@ -41,7 +39,7 @@ void readFiles(struct hashmap* hm)
             break;
         }
         hm_put(hm,word,0,1,0);
-        char* word = strtok(NULL," ");
+        word = strtok(NULL," ");
     }
     fclose(fptr);//close file 2
     fptr = fopen("D3.txt", "r");//open file 3
@@ -58,18 +56,40 @@ void readFiles(struct hashmap* hm)
             break;
         }
         hm_put(hm,word,0,0,1);
-        char* word = strtok(NULL," ");
+        word = strtok(NULL," ");
     }
     fclose(fptr);
 }
 
 int main(void)
 {
+    printf("Enter the number of buckets you want the hashmap to have: \n");
+    int numBuckets;
+    scanf("%i", &numBuckets);
+    struct hashmap *hm = hm_create(numBuckets);
+    printf("created hashmap of size %i\n",numBuckets);
+    //training(hm);
+    while(1)
+    {
+        printf("Enter S to search and X to exit: \n");
+        char choice;
+        scanf("%c", &choice);
+        if(choice == 'S')
+        {
+            //read_query();
+        }
+        else if(choice == 'X')
+        {
+            break;
+        }
+    }
+    training(hm);
+    
+    /*
     printf("___________________________________________________________\n");
     printf("Create the hashmap and input files:\n");
     struct hashmap *hm = hm_create(23);
     printf("I made a hashmap\n");
-    readFiles(hm);
-    printf("___________________________________________________________\n");
+    training(hm);
+    printf("___________________________________________________________\n");*/
 }
-
